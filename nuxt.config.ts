6ea97f3547d915705f7aedf7ed2006c483b23745
FikFikk <fikri225456@gmail.com>
2025-09-17 21:33:49 +0700

    setup github actions for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/p/',
+  },
+  ssr: false,
+  nitro: {
+    preset: 'github-pages'
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
